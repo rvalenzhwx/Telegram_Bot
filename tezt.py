e606b55b0642693604ce30fd6fc300dac827926f
Rodrigo Valenzuela <rvalenzuela@gmail.com>
2023-05-22 09:29:22 -0400

    simple-request.py, working exampler using request lib

--- tezt.py
+++ tezt.py
@@ -1,6 +1,16 @@
-#!/usr/bin/env -S python -i
+#!/usr/bin/env python3
 
-lsa="1"
-lsb=1
+import keyring
+import keyring.util.platform_ as keyring_platform
 
-print(lsa, lsb)
+print(keyring_platform.config_root())
+print(keyring.get_keyring())
+
+SERV="dedfded"
+TOK="efdefefefe"
+
+keyring.set_password(SERV, TOK, "3-python3-jeepney_0.8.0-3_all.deb")
+cred=keyring.get_credential(SERV, TOK)
+print(cred.username)
+print(cred.password)
+
